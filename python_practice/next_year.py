e6b3c139baf5497512027cd32a20f6c5f678f878
sxlvinz <sxlvinz@gmail.com>
2025-02-12 01:45:04 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,7 @@
+print ("Hello, World!")
+name = input("Enter your name: ")
+print("Hello + name")
+print(f"Hello {name}")
+age = int(input("Enter your age: "))
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Next year you will be {age + 1} years old.")
